d409586ff3685b89cbff9a74dd4eb56a9cde68ff
tanv shinde <shindetanvi.com>
2022-12-19 17:57:28 +0530

    programs

---
+++ welcom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace empprogam
+{
+    public class welcom
+
+    {
+
+
+        public static void displaymsg()
+        {
+            Console.WriteLine("Welcome To Employee Problem");
+        }
+    }
+}
